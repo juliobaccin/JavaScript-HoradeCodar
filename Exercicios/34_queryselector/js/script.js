374efa769467ac25677cfb596bae6d14fd277382
Julio Cesar Baccin <juliobassin@hotmail.com>
2022-05-23 22:24:36 -0300

    OK

---
+++ Exercicios/34_queryselector/js/script.js
@@ -0,0 +1,21 @@
+//querySelectorAll - pega todos os elementos
+
+//Pegando todos os elementos com a classe item
+var itensClasse = document.getElementsByClassName('item')
+console.log(itensClasse)
+
+//Pegando todos os itens dentro da lista do elemento com id listas2
+var intesQuery = document.querySelectorAll('#lista2 li')
+console.log(intesQuery)
+
+//Peganndo todos os elementos dentro da lista do elemento com id lista
+var itensQuery2 = document.querySelectorAll('#lista li')
+console.log(itensQuery2)
+
+//querySelector - pega 
+
+var primeiraLista = document.querySelector('ul')
+console.log(primeiraLista)
+
+var span = document.querySelector('#paragrafo span')
+console.log(span)
